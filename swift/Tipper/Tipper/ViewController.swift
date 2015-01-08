7ff436e748e49d8ed003ed2d4ee57a3d38d7094b
Emmanuel Texier <emmanuel.texier@magnet.com>
2015-01-08 00:11:53 -0800

    Add Swift project

---
+++ swift/Tipper/Tipper/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Emmanuel Texier on 12/22/14.
+//  Copyright (c) 2014 Emmanuel Texier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Settings", style: UIBarButtonItemStyle, target: self, action:@selector(onSettingsButton))
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.15, 0.18, 0.2]
+        var billAmount = (billField.text as NSString).doubleValue
+        var tip = billAmount * tipPercentages[tipControl.selectedSegmentIndex]
+        var total = billAmount + tip
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
